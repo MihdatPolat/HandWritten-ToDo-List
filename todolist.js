92c99c773d5a9fa8464d31140669ec98cf097c08
Mihdat Polat <mihdatzeki85@gmail.com>
2023-01-23 04:41:43 +0300

    second commit

--- todolist.js
+++ todolist.js
@@ -1,5 +1,8 @@
 (() =>{
-    let toDoListArray = [];
+    var toDoListArray = [];
+    // Sayfayı yüklediğimizde localStorage'daki verileri okuyoruz
+    let storedList = JSON.parse(localStorage.getItem("toDoList")) || [];
+    toDoListArray = storedList;
     const form = document.querySelector(".form");
     const input = form.querySelector(".form__input");
     const ul =document.querySelector(".toDoList");
@@ -8,8 +11,10 @@
         e.preventDefault();
         let ItemId= String(Date.now());
         let toDoItem= input.value;
-        addItemToDOM(ItemId, toDoItem);
-        addItemTOArray(ItemId, toDoItem);
+        addItemToDOM(ItemId, toDoItem, false);
+        addItemTOArray(ItemId, toDoItem, false);
+        // Liste güncellendiğinde localStorage'a kaydediyoruz
+        localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
         input.value='';
     });
     ul.addEventListener('click', toggleDone);
@@ -17,16 +22,31 @@
     function toggleDone(e) {
         let id = e.target.getAttribute('data-id');
         let li = document.querySelector('[data-id="' + id + '"]');
-        li.classList.toggle("strike-through");
+        let index = toDoListArray.findIndex(item => item.ItemId === id);
+        toDoListArray[index].strike = !toDoListArray[index].strike;
+        li.classList.toggle("strike-through", toDoListArray[index].strike);
+        // Liste güncellendiğinde localStorage'a kaydediyoruz
+        localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
     }
     let firstItem = true;
     const respectLi = document.createElement('li');
         respectLi.innerText = 'Saygınlık';
         
-    function addItemToDOM(ItemId, toDoItem) {
+     // Sayfa yüklendiğinde localStorage'daki verileri ekliyoruz
+     for (let i = 0; i < storedList.length; i++) {
+        let id = storedList[i].ItemId;
+        let toDoItem = storedList[i].toDoItem;
+        let strike = storedList[i].strike;
+        addItemToDOM(id, toDoItem, strike);
+    }
+
+    function addItemToDOM(ItemId, toDoItem, strike) {
         const li = document.createElement('li')
         li.setAttribute("data-id", ItemId);
-        li.innerText =toDoItem;
+        li.innerText = toDoItem;
+        if (strike) {
+            li.classList.add("strike-through");
+        }
         ul.appendChild(li);
         const deleteIcon = document.createElement('button');
         deleteIcon.innerHTML = "X";
@@ -37,6 +57,11 @@
         removeItemFromDom(ItemId);
         removeItemFromArray(ItemId);
     });
+    if(toDoListArray.length >=5){
+        ul.appendChild(respectLi);
+        localStorage.setItem("respectLi", JSON.stringify(respectLi));
+    }
+    /*
     if (firstItem) {
         firstItem = false;
         ul.appendChild(respectLi);
@@ -44,12 +69,12 @@
     else{
         ul.insertBefore(li,respectLi);
 
-    }
+    }*/
     }
     
     
-    function addItemTOArray(ItemId, toDoItem) {
-        toDoListArray.push({ItemId, toDoItem});
+    function addItemTOArray(ItemId, toDoItem, strike) {
+        toDoListArray.push({ ItemId, toDoItem, strike });
         console.log(toDoListArray)
     }
     
@@ -57,11 +82,20 @@
         var li = document.querySelector('[data-id="' + id + '"]')
         ul.removeChild(li);
         li.classList.remove("strike-through");
+        // Liste güncellendiğinde localStorage'a kaydediyoruz
+        localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
 
     }
     function removeItemFromArray(id) {
         toDoListArray = toDoListArray.filter(item => item.ItemId !== id);
-        console.log(toDoListArray);
+        if(toDoListArray.length == 0){
+            if(ul.contains(respectLi)){
+                ul.removeChild(respectLi);
+                localStorage.removeItem("respectLi");
+            }
+        }
+         // Liste güncellendiğinde localStorage'a kaydediyoruz
+         localStorage.setItem("toDoList", JSON.stringify(toDoListArray));
     }
 
     let docTitle= document.title;
